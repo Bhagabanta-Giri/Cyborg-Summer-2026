46f7feb9381083ec743f93f94e43f75b08c6a66e
Bhagabanta-Giri <bhagabantagiri@gmail.com>
2026-06-06 00:48:56 +0530

    Removed ROS2 build, install, log folders from tracking

--- Task3/build/task3/CMakeFiles/task3__rosidl_generator_type_description.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/task3__rosidl_generator_type_description"
-  "rosidl_generator_type_description/task3/srv/PrimeFactors.json"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/task3__rosidl_generator_type_description.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Task3/build/task3/task3__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lenovoi/Projects/Cyborg-Summer-Induction-2026/Cyborg-Summer-2026/Task3/build/task3/task3__py
-# Build directory: /home/lenovoi/Projects/Cyborg-Summer-Induction-2026/Cyborg-Summer-2026/Task3/build/task3/task3__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
