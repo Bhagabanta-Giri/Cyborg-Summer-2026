090d665312a0f97b7df4709b3fa42c1cd3fe765c
Bhagabanta-Giri <bhagabantagiri@gmail.com>
2026-06-07 12:58:44 +0530

    Task3D done

--- Task3/src/task3/scripts/Task3D_client.py
+++ Task3/src/task3/scripts/Task3D_client.py
@@ -1,3 +1,45 @@
 #!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from task3.srv import PrimeFactors
+from functools import partial
+import sys
+
+class client_node(Node):
+    def __init__(self):
+        super().__init__("client_node")
+
+    def call_prime_factors_service(self, a):
+        client = self.create_client(PrimeFactors, "/prime_factors")
+
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for service...")
+
+        request = PrimeFactors.Request()
+        request.a = a
+
+        future = client.call_async(request)
+
+        future.add_done_callback(partial(self.callback_prime_factors))
+    
+    def callback_prime_factors(self, future):
+        try:
+            response = future.result()
+            factors_list = list(response.prime_factors)
+            self.get_logger().info(f"Result Received!\nPrimeFactors: {factors_list}")
+        except Exception as e:
+            self.get_logger().error(f"Service call failed: {e}")
+        pass
+
+def main(args = None):
+    rclpy.init(args=args)
+    input_a = int(sys.argv[1])
+    node = client_node()
+    node.call_prime_factors_service(input_a)
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
 
 # Write your client implementation here.
--- Task3/src/task3/scripts/Task3D_server.py
+++ Task3/src/task3/scripts/Task3D_server.py
@@ -1,3 +1,34 @@
 #!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from task3.srv import PrimeFactors
 
+
+class server_node(Node):
+    def __init__(self):
+        super().__init__("prime_factors_server")
+        self.srv = self.create_service(PrimeFactors, 'prime_factors', self.handle_prime_factors)
+
+    def handle_prime_factors(self, request, response):
+        a = request.a
+        factors = []
+        divisor = 2
+        while a > 1:
+            if a % divisor == 0:
+                factors.append(divisor)
+                a //= divisor
+            else:
+                divisor += 1
+        self.get_logger().info(f'Prime factors of {request.a} are: {factors}')
+        response.prime_factors = factors
+        return response
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = server_node()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
 # Write your server implementation here.
